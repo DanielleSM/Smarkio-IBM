b2e3e21dcd59ddd3b2072dfe09957be4cc697fd5
DanielleSM <69157307+DanielleSM@users.noreply.github.com>
2020-12-21 17:10:29 -0300

    Update api-config.js

--- config/api-config.js
+++ config/api-config.js
@@ -1,8 +1,8 @@
 var config = {};
 
-config.apikey = "t-v0WeIBeVzhQOnQUFsLv41SoZ-tzU5yYi8Z0m0wci6U";
+config.apikey = "your-API key";
 
-config.ibmUrl = "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/84cad1a4-a739-403e-bec1-327d456d2989";
+config.ibmUrl = "your-URL";
 
 config.voice="pt-BR_IsabelaVoice";
 
